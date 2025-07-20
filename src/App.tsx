b4e00c5a1689058a324c6a7dbafd420bb31c5712
VariantBSP <91957967+VariantBSP@users.noreply.github.com>
2025-07-19 23:24:30 -1100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListPage from "./components/ListPage";
+import Home from "./components/Home";
+import Booking from "./components/Booking";
+import Thanks from "./components/Thanks";
+import NotFound from "./components/NotFound";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="booking" element={<Booking />} />
+                <Route path="listpage" element={<ListPage />} />
+                <Route path="Thanks" element={<Thanks />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+  );
+}
+
+export default App;
